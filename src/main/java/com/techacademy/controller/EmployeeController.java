ff1b0b20739bb3224496b08f76d9f19bf0fb2e41
GSVolpato91 <guido.svolpato@gmail.com>
2024-02-27 16:30:55 +0900

    Fixed employee controller.

--- src/main/java/com/techacademy/controller/EmployeeController.java
+++ src/main/java/com/techacademy/controller/EmployeeController.java
@@ -111,11 +111,12 @@ public class EmployeeController {
     @PostMapping("/{code}/update")
     public String update(@Validated Employee employee, BindingResult res, @PathVariable("code") String code,
             Model model) {
-
         Employee existingEmployee = employeeService.findByCode(code);
         ErrorKinds result = employeeService.update(employee, existingEmployee);
-
         if (res.hasErrors()) {
+            return edit(null, employee, model);
+        }
+        if (ErrorMessage.contains(result)) {
             model.addAttribute(ErrorMessage.getErrorName(result), ErrorMessage.getErrorValue(result));
             return edit(null, employee, model);
         }
